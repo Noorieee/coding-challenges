32ea2de83dd6c2e78587b3f26fe04c4c81b6934a
Noor <hussainmahnoor112@gmail.com>
2025-04-14 10:41:24 +0100

    Updated package, storybook preview file and added an image

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,17 @@ import type { Preview } from '@storybook/react'
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      values: [
+        // 👇 Default values
+        { name: 'Dark', value: '#333' },
+        { name: 'Light', value: '#F7F9F2' },
+        // 👇 Add your own
+        { name: 'Maroon', value: '#400' },
+      ],
+      // 👇 Specify which background is shown by default
+      default: 'Dark',
+    },
     controls: {
       matchers: {
        color: /(background|color)$/i,
