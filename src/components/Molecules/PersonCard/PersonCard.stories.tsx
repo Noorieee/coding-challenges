fe07883fd0fb4884a5581c70172f65ac37ce0166
Noorieee <hussainmahnoor112@gmail.com>
2025-04-16 15:37:38 +0100

    Created person card component which is also using the Avatar component

---
+++ src/components/Molecules/PersonCard/PersonCard.stories.tsx
@@ -0,0 +1,102 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import PersonCard from './index'
+
+const meta: Meta<typeof PersonCard> = {
+  title: 'Miscellaneous/PersonCard',
+  component: PersonCard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    name: {
+      control: 'text',
+      description: 'Name of the person',
+      type: {
+        name: 'string',
+        required: true
+      }
+    },
+    email: {
+      control: 'text',
+      description: 'Email of the person',
+      type: {
+        name: 'string',
+        required: true
+      }
+    },
+    imageUrl: {
+      control: 'text',
+      description: 'URL of avatar image'
+    },
+    vanity: {
+      control: 'boolean',
+      description: 'Vanity flag',
+      table: {
+        defaultValue: { summary: 'false' },
+      },
+    },
+    isHidden: {
+      control: 'boolean',
+      description: 'Whether to hide the avatar, email, and name',
+      table: {
+        defaultValue: { summary: 'false' },
+      },
+    },
+    initial: {
+      control: 'text',
+      description: 'The initial of the person',
+      type: {
+        name: 'string',
+        required: true
+      }
+    }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof PersonCard>
+
+export const Default: Story = {
+  args: {
+    name: 'Noor Hussain',
+    email: 'noor.hussain@example.com',
+    imageUrl: 'https://www.wboy.com/wp-content/uploads/sites/43/2024/02/North_American_river_otter.jpg',
+    vanity: false,
+    isHidden: false,
+    initial: 'N'
+  }
+}
+
+export const WithVanity: Story = {
+  args: {
+    name: 'Noor Hussain',
+    email: 'noor.hussain@example.com',
+    imageUrl: 'https://www.wboy.com/wp-content/uploads/sites/43/2024/02/North_American_river_otter.jpg',
+    vanity: true,
+    isHidden: false,
+    initial: 'N'
+  }
+}
+
+export const WithInitial: Story = {
+  args: {
+    name: 'Noor Hussain',
+    email: 'noor.hussain@example.com',
+    imageUrl: '',
+    vanity: true,
+    isHidden: false,
+    initial: 'N'
+  }
+}
+
+export const HiddenCard: Story = {
+  args: {
+    name: 'Noor Hussain',
+    email: 'noor.hussain@example.com',
+    imageUrl: 'https://www.wboy.com/wp-content/uploads/sites/43/2024/02/North_American_river_otter.jpg',
+    vanity: false,
+    isHidden: true,
+    initial: 'N'
+  }
+}
---
+++ src/components/Molecules/PersonCard/index.tsx
@@ -0,0 +1,123 @@
+import React from 'react'
+import Avatar from '../../Atoms/Avatar'
+import styled from '@emotion/styled'
+
+const PersonCardContainer = styled.div(
+  () => {
+    return `
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: center;
+      gap: 10px;
+      height: 100px;
+      font-family: Open Sans, sans-serif;
+      position: relative;
+      background-color: #f0f0f0;
+    `
+  }
+)
+
+const AvatarContainer = styled.div(
+  () => {
+    return `
+      position: absolute;
+      left: 20px;
+      top: 50px;
+      transform: translate(-50%, -50%);
+      z-index: 1;
+    `
+  }
+)
+
+const PersonDetailsContainer = styled.div<{ isHidden?: boolean }>(
+  ({ isHidden }) => {
+    let colors = `
+      background-color: #f0f0f0;
+    `
+    if (isHidden) {
+      colors = `
+        background-color: #84c489;
+      `
+    }
+    return `
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+      margin-left: 16px;
+      gap: 8px;
+      ${colors};
+      padding: 30px 95px;
+      width: 100%;
+      margin: 0;
+    `
+  }
+)
+
+const PersonName = styled.h1(
+  () => {
+    return `
+      font-size: 32px;
+      font-weight: 600;
+      margin: 0px;
+      width: 100%;
+    `
+  }
+)
+
+const PersonEmail = styled.p(
+  () => {
+    return `  
+      font-size: 14px;
+      font-weight: 400;
+      margin: 0px;
+      width: 100%;
+    `
+  }
+)
+
+const HiddenDetail = styled.div<{ isSmall?: boolean }>(
+  ({ isSmall }) => {
+    return `
+      margin: 0px;
+      width: 100%;
+      min-width: 150px;
+      width: 100%;
+      height: ${isSmall ? '16px' : '30px'};
+      background-color: #a5e0aa;
+    `
+  }
+)
+
+interface PersonCardProps {
+  name: string
+  email: string
+  imageUrl?: string
+  vanity?: boolean
+  isHidden?: boolean
+  initial: string
+}
+
+const PersonCard = ({ name, email, imageUrl, vanity, isHidden, initial }: PersonCardProps) => {
+  return (
+    <PersonCardContainer>
+      <AvatarContainer><Avatar initial={initial} imageUrl={imageUrl} isHidden={isHidden} vanity={vanity} /></AvatarContainer>
+      <PersonDetailsContainer isHidden={isHidden}>
+        {isHidden ? (
+          <>
+            <HiddenDetail />
+            <HiddenDetail isSmall />
+          </>
+        ) : (
+          <>
+            <PersonName>{name}</PersonName>
+            <PersonEmail>{email}</PersonEmail>
+          </>
+        )}
+      </PersonDetailsContainer>
+    </PersonCardContainer>
+  )
+}
+
+export default PersonCard
